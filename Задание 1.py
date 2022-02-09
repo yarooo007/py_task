72ac8979751cc08a84cfe3f5bd22fdb2e900532c
KY <kordin@dlit.dp.ua>
2022-02-09 17:17:49 +0300

    Ready

--- Задание 1.py
+++ Задание 1.py
@@ -1,5 +1,3 @@
-a = input("")
-if int(a[0])+int(a[1])==int(a[2])+int(a[3]):
-    print("Число является счасливым билетом")
-else:
-    print("Неудача:(")
+a = set(input("Первая строка "))
+b = set(input("Вторая строка "))
+print(a.intersection(b))
